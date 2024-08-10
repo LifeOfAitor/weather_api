bec65f1a80764d05b3d1aaf49e7a6c5577de831c
LifeOfAitor <aitorgaillard99@gmail.com>
2024-08-10 09:36:10 +0200

    return temperature for any statation and date

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
+
+def reformat(date, station, temperature):
+    return {
+        "date": date,
+        "station": station,
+        "temperature": temperature
+    }
 
 
 @app.route("/")
@@ -8,5 +17,16 @@ def home():
     return render_template("home.html")
 
 
-if __name__ == "--main__":
+@app.route("/api/v1/<station>/<date>")
+def translator(station, date):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    # read the data
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+    # prepare data to render
+    data = reformat(date, station, temperature)
+    return render_template("output.html", data=data)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
